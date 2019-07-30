17bfb29ddd00064f507e962ebd0ef6052d06e7c3
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2019-07-30 15:19:24 +0200

    Cleanup project to be minimal

---
+++ xc11b5t6-bug/A.swift
@@ -0,0 +1,15 @@
+//
+//  A.swift
+//  xc11b5t6-bug
+//
+//  Created by Hendrik Ulbrich on 30.07.19.
+//  Copyright © 2019 Hendrik Ulbrich. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class A: ObservableObject {
+    @Published var element: String? = nil
+}
---
+++ xc11b5t6-bug/B.swift
@@ -0,0 +1,15 @@
+//
+//  B.swift
+//  xc11b5t6-bug
+//
+//  Created by Hendrik Ulbrich on 30.07.19.
+//  Copyright © 2019 Hendrik Ulbrich. All rights reserved.
+//
+
+import Foundation
+
+extension A {
+    func evil() {
+        element = ""
+    }
+}
